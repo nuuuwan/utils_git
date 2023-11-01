0637fd60d239f5c43d3b3f898348ca59c942474b
nuuuwan <nuuuwan@gmail.com>
2023-11-01 11:27:27 +0530

    Test Commit

--- src/utils_git/Git.py
+++ src/utils_git/Git.py
@@ -1,6 +1,69 @@
-from utils_git.GitReadOnly import GitReadOnly
-from utils_git.GitWrite import GitWrite
+import os
+import shutil
+
+from utils_base import Log
+
+log = Log('Git')
 
 
-class Git(GitReadOnly, GitWrite):
-    pass
+class Git:
+    def __init__(self, git_repo_url):
+        self.git_repo_url = git_repo_url
+        self.dir_repo = None
+        self.branch_name = None
+
+    def run(self, *cmd_lines):
+        assert self.dir_repo is not None
+        cmd = ' '.join(cmd_lines)
+        log.debug(cmd)
+        os.system('cd ' + self.dir_repo)
+        os.system(cmd)
+
+    # Initialization
+    def clone(self, dir_repo, branch_name):
+        assert dir_repo is not None
+        assert branch_name is not None
+
+        self.dir_repo = dir_repo
+        self.branch_name = branch_name
+
+        if os.path.exists(dir_repo):
+            shutil.rmtree(dir_repo)
+            log.debug(f'Removed {dir_repo}')
+        os.makedirs(dir_repo)
+        log.debug(f'Created {dir_repo}')
+
+        self.run(
+            'git clone',
+            f'-b {self.branch_name}',
+            '--single-branch ',
+            self.git_repo_url,
+            self.dir_repo,
+        )
+
+    # Working on Code
+    def status(self):
+        return self.run('git status')
+
+    def diff(self):
+        return self.run('git diff', '--compact-summary')
+
+    # Staging Changes
+    def add(self):
+        return self.run('git add', '.')
+
+    # Committing Changes
+    def commit(self, message):
+        return self.run('git commit', f'-m "{message}"')
+
+    # Synchronization
+    def pull(self):
+        return self.run('git pull origin', self.branch_name)
+
+    def push(self):
+        return self.run('git push origin', self.branch_name)
+
+    # Branching
+    def checkout(self, branch_name):
+        self.branch_name = branch_name
+        return self.run('git checkout', self.branch_name)
--- tests/test_git.py
+++ tests/test_git.py
@@ -2,9 +2,11 @@ import os
 import tempfile
 import unittest
 
+from utils_base import TIME_FORMAT_TIME_ID, File, Time
+
 from utils_git.Git import Git
 
-TEST_REPO_URL = 'https://github.com/nuuuwan/utils'
+TEST_REPO_URL = 'https://github.com/nuuuwan/utils_git'
 TEST_DIR_REPO = tempfile.TemporaryDirectory().name
 TEST_BRACH_NAME = 'main'
 
@@ -12,38 +14,24 @@ TEST_GIT = Git(TEST_REPO_URL)
 
 
 class TestGit(unittest.TestCase):
-    def test_clone(self):
+    def test_lifecycle(self):
         git = TEST_GIT
         git = Git(TEST_REPO_URL)
-        git.clone(TEST_DIR_REPO, force=True)
+        git.clone(TEST_DIR_REPO, TEST_BRACH_NAME)
 
-    @unittest.skip('Fails on windows')
-    def test_checkout(self):
-        git = TEST_GIT
-        git.clone(TEST_DIR_REPO, force=True)
-        git.checkout(TEST_BRACH_NAME)
-        self.assertTrue(os.path.exists(TEST_DIR_REPO))
+        git.status()
+        git.diff()
 
-        git.clone(TEST_DIR_REPO, force=False)
-        git.checkout(TEST_BRACH_NAME)
+        test_file = os.path.join(TEST_DIR_REPO, 'tests', 'test.txt')
+        File(test_file).write(TIME_FORMAT_TIME_ID.stringify(Time.now()))
 
-    def test_add_and_commit(self):
-        git = TEST_GIT
-        git.clone(TEST_DIR_REPO, force=True)
-        git.checkout(TEST_BRACH_NAME)
-        cmd = f'echo "test" > {TEST_DIR_REPO}/test.txt'
-        os.system(cmd)
-        git.add_and_commit('test')
+        git.add()
+        git.commit('Test Commit')
 
-    @unittest.skip('Changes repo code')
-    def test_push(self):
-        git = TEST_GIT
-        git.clone(TEST_DIR_REPO, force=True)
-        git.checkout(TEST_BRACH_NAME)
-        cmd = f'echo "$(date +%Y%m%d-%H%M%S)" > {TEST_DIR_REPO}/test.txt'
-        os.system(cmd)
-        git.add_and_commit('test')
+        git.pull()
         git.push()
+
+        git.checkout(TEST_BRACH_NAME)
 
 
 if __name__ == '__main__':
