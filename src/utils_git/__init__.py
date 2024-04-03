5f4a55c75fc90e11fe186a618ab0001a5f3c48e9
nuuuwan <nuuuwan@gmail.com>
2024-04-03 17:25:23 +0530

    Removed KeyValueStore

--- src/utils_git/__init__.py
+++ src/utils_git/__init__.py
@@ -1,4 +1,5 @@
 # utils_git (auto generate by build_inits.py)
 
-from utils_git.core import Git
-from utils_git.kvs import Key, KeyValueStore
+from utils_git.core import (
+    Git,
+)
--- src/utils_git/kvs/__init__.py
+++
@@ -1,4 +0,0 @@
-# utils_git.kvs (auto generate by build_inits.py)
-
-from utils_git.kvs.Key import Key
-from utils_git.kvs.KeyValueStore import KeyValueStore
